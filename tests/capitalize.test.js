c65cbf171e7907eaacb2457a4b6c725f8336a7bf
Ruslan Ayupov <rus779@me.com>
2023-09-03 09:36:39 +0300

    add assert-test

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,11 +1,6 @@
+import { strict as assert } from 'node:assert';
+
 import capitalize from '../scr/capitalize.js';
 
-if (capitalize('hello') !== 'Hello') {
-    throw new Error('Ошибка в работе функции!');
-}
-
-if (capitalize('') !== '') {
-    throw new Error('Ошибка в работе с пустой строкой');
-}
-
-console.log('Пройдены проверки');
+assert.deepEqual(capitalize(''), '');
+assert.deepEqual(capitalize('hello'), 'Hello');
